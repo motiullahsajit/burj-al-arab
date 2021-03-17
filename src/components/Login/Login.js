3159eed2093a16df42a2a6cf5885b4e43329649f
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-03-18 02:19:52 +0600

    done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,40 +1,18 @@
 import React, { useContext, useState } from 'react';
-import firebase from "firebase/app";
-import "firebase/auth";
-import { firebaseConfig } from './firebase.config';
 import { UserContext } from "../../App";
 import { useHistory, useLocation } from 'react-router';
+import { createUserWithEmailAndPassword, googleSingIn, initializeLoginFramework, signInWithEmailAndPassword, handleSignOut, facebookSingIn, } from './LoginManager';
 
-
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-} else {
-    firebase.app();
-}
 
 const Login = () => {
+    initializeLoginFramework();
     const [loggedInUser, setLoggedInUser] = useContext(UserContext)
-    const provider = new firebase.auth.GoogleAuthProvider();
     let history = useHistory();
     let location = useLocation();
     let { from } = location.state || { from: { pathname: "/" } };
     const [error, setError] = useState()
-    const handleGoogleSingIn = () => {
-        firebase.auth()
-            .signInWithPopup(provider)
-            .then((result) => {
-                const { displayName, email } = result.user;
-                const singnedInUser = { name: displayName, email: email }
-                setLoggedInUser(singnedInUser)
-                history.replace(from);
-
-            }).catch((error) => {
-                const errorMessage = error.message;
-                setError(errorMessage)
-            });
-    }
     const [formData, setFormData] = useState({ email: null, password: null });
-    const [option, setOption] = useState('register');
+    const [option, setOption] = useState('login');
 
     const handleBlur = (e) => {
         let isFieldValid = true;
@@ -59,58 +37,73 @@ const Login = () => {
 
     const signUp = (e) => {
         e.preventDefault();
-        firebase.auth().createUserWithEmailAndPassword(formData.email, formData.password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                setLoggedInUser(user);
-                history.replace(from);
-            })
-            .catch((error) => {
-                const errorMessage = error.message;
-                setError(errorMessage)
-            });
+        createUserWithEmailAndPassword(formData.email, formData.password).then(res => {
+            handleResponse(res, true)
+        })
     }
 
 
     const signIn = (e) => {
         e.preventDefault();
+        signInWithEmailAndPassword(formData.email, formData.password).then(res => {
+            handleResponse(res, true)
+        })
+    }
 
-        firebase.auth().signInWithEmailAndPassword(formData.email, formData.password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                setLoggedInUser(user);
-                history.replace(from);
-            })
-            .catch((error) => {
-                const errorMessage = error.message;
-                setError(errorMessage)
-            });
+    const singOUt = (e) => {
+        handleSignOut().then(res => {
+            handleResponse(res, false)
+        })
+    }
 
+    const handleGoogleSingIn = () => {
+        googleSingIn().then(res => {
+            handleResponse(res, true)
+        })
+    }
+    const handleFbSingIn = () => {
+        facebookSingIn().then(res => {
+            handleResponse(res, true)
+        })
+    }
+
+    const handleResponse = (res, redirect) => {
+        setLoggedInUser(res)
+        redirect && history.replace(from);
     }
 
 
     return (
         <>
             {
-                loggedInUser.email ? <> <p>{loggedInUser.name}</p> <p>Your are already logged in</p> <br /> <button onClick={() => setLoggedInUser('')}>Sign Out</button></>
+                loggedInUser.email ? <div className='container col-md-3 text-center'> <h3>{loggedInUser.name}</h3>  <h3>Email: {loggedInUser.email}</h3> <h5>Your are already logged in</h5> <button className='btn btn-success w-100 my-1' onClick={singOUt}>Sign Out</button></div>
                     : <>
-                        <div style={{ textAlign: 'center' }}>
-                            <h1>This is Login</h1>
-                            <p>{error}</p>
-                            <button onClick={handleGoogleSingIn}>Google Sing In</button>
-                            <h1>Or</h1>
-                            <button onClick={() => setOption('register')} className={`btn ${option === 'register' ? 'btn-primary' : 'btn-light'}  col`}>Register</button>
-                            <button onClick={() => setOption('login')} className={`btn ${option === 'login' ? 'btn-primary' : 'btn-light'}  col`}>Login</button>
-                            <form>
-                                <input type="text" onBlur={(e) => handleBlur(e)} name='email' placeholder='Your Email' required />
-                                <br />
-                                <input type="password" onBlur={(e) => handleBlur(e)} name='password' placeholder='Your Password' required />
-                                <br />
-                                {
-                                    option === 'register' ? <input type='submit' onClick={signUp} value='Sign Up' /> :
-                                        <input type='submit' onClick={signIn} value='Sign In' />
-                                }
+                        <div className="col-md-3 mt-5 container">
+                            <div className="tab bg-light p-2 rounded mb-3 row">
+                                <button onClick={() => setOption('register')} className={` ${option === 'register' ? 'btn-primary' : 'btn-light'}  col`}>Register</button>
+                                <button onClick={() => setOption('login')} className={`btn ${option === 'login' ? 'btn-primary' : 'btn-light'}  col`}>Login</button>
+                            </div>
+                            <form className="form my-4">
+                                <div className="mb-3">
+                                    <input type="text" onBlur={(e) => handleBlur(e)} name='email' placeholder='Your Email' required className="form-control" />
+                                </div>
+                                <div className="mb-3">
+                                    <input type="password" onBlur={(e) => handleBlur(e)} name='password' placeholder='Your Password' required className="form-control" />
+                                </div>
+                                <div className="mb-3 form-check">
+                                    <input type="checkbox" className="form-check-input" />
+                                    <label className="form-check-label">Remember me</label>
+                                </div>
+                                <div className="mb-3 d-grid">
+                                    {
+                                        option === 'register' ? <input type='submit' className='btn btn-primary' onClick={signUp} value='Sign Up' /> :
+                                            <input type='submit' className='btn btn-primary' onClick={signIn} value='Sign In' />
+                                    }
+                                    <p>{error}</p>
+                                </div>
                             </form>
+                            <button className='btn btn-success w-100 my-1' onClick={handleGoogleSingIn}>Google Sing In</button>
+                            <button className='btn btn-primary w-100 my-1' onClick={handleFbSingIn}>Facebook Sing In</button>
                         </div>
                     </>
             }
